5580142a291bdf524bb6c44ab11aa6a5ab4acaf3
ChienkuChen <chienku_chen@htc.com>
2018-01-17 19:53:21 +0800

    Response should be json format

--- forex/src/main/scala/forex/interfaces/api/utils/ApiExceptionHandler.scala
+++ forex/src/main/scala/forex/interfaces/api/utils/ApiExceptionHandler.scala
@@ -1,50 +1,49 @@
 package forex.interfaces.api.utils
 
 import akka.http.scaladsl._
-import akka.http.scaladsl.model._
+import akka.http.scaladsl.model.StatusCodes
+import akka.http.scaladsl.server.Directives._
+import com.typesafe.scalalogging.LazyLogging
+import de.heikoseeberger.akkahttpcirce.ErrorAccumulatingCirceSupport._
 import forex.processes._
+import io.circe.generic.semiauto.{ deriveDecoder, deriveEncoder }
+import io.circe.{ Decoder, Encoder }
 
-final case class ErrorRespone(reason: String, throwable: Throwable)
+final case class ExceptionResponse(reason: String, throwableMessage: String)
+object ExceptionResponse {
+  @SuppressWarnings(Array("org.wartremover.warts.PublicInference"))
+  implicit val encoder: Encoder[ExceptionResponse] =
+    deriveEncoder[ExceptionResponse]
 
-object ApiExceptionHandler {
+  @SuppressWarnings(Array("org.wartremover.warts.PublicInference"))
+  implicit val decoder: Decoder[ExceptionResponse] =
+    deriveDecoder[ExceptionResponse]
+}
+
+object ApiExceptionHandler extends LazyLogging {
 
   def apply(): server.ExceptionHandler =
     server.ExceptionHandler {
       case f: RatesError.ForgeError ⇒
-        ctx ⇒
-          ctx.complete(
-            HttpResponse(
-              StatusCodes.InternalServerError,
-              entity = HttpEntity(
-                s"""{"reason":"forger service error: ${f.reason}", "throwable":"${f.throwable}"}"""
-              )
-            )
-          )
+        val response = ExceptionResponse(s"forge service error: ${f.reason}", f.throwable.fold("")(_.getMessage))
+        logger.error("forge service error", f.throwable)
+        complete((StatusCodes.InternalServerError, response))
+
       case c: RatesError.CacheError ⇒
-        ctx ⇒
-          ctx.complete(
-            HttpResponse(
-              StatusCodes.InternalServerError,
-              entity = HttpEntity(s"""{"reason":"cache error", "throwable":"${c.throwable}"}""")
-            )
-          )
+        val response = ExceptionResponse("cache error:", c.throwable.getMessage)
+        logger.error("cache error", c.throwable)
+        complete((StatusCodes.InternalServerError, response))
+
       case u: RatesError.UnexpectedError ⇒
-        ctx ⇒
-          ctx.complete(
-            HttpResponse(
-              StatusCodes.InternalServerError,
-              entity = HttpEntity(s"""{"reason":"unexpected error", "throwable":"${u}"}""")
-            )
-          )
+        val response = ExceptionResponse(s"unexpected error: ${u.reason}", u.throwable.fold("")(_.getMessage))
+        logger.error("unexpected error", u.throwable)
+        complete((StatusCodes.InternalServerError, response))
+
       case t: Throwable ⇒
-        ctx ⇒
-          t.printStackTrace()
-          ctx.complete(
-            HttpResponse(
-              StatusCodes.InternalServerError,
-              entity = HttpEntity(s"""{"reason":"unexpected error", "throwable":"${t}"}""")
-            )
-          )
+        val response =
+          ExceptionResponse(s"unexpected error", t.getMessage)
+        logger.error("unexpected error", t)
+        complete((StatusCodes.InternalServerError, response))
     }
 
 }
--- forex/src/main/scala/forex/interfaces/api/utils/ApiRejectionHandler.scala
+++ forex/src/main/scala/forex/interfaces/api/utils/ApiRejectionHandler.scala
@@ -1,10 +1,52 @@
 package forex.interfaces.api.utils
 
 import akka.http.scaladsl._
+import akka.http.scaladsl.model.StatusCodes
+import akka.http.scaladsl.model.StatusCodes.NotFound
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.{ RejectionHandler, _ }
+import com.typesafe.scalalogging.LazyLogging
+import de.heikoseeberger.akkahttpcirce.ErrorAccumulatingCirceSupport._
+import io.circe.generic.semiauto.{ deriveDecoder, deriveEncoder }
+import io.circe.{ Decoder, Encoder }
 
-object ApiRejectionHandler {
+final case class RejectionResponse(reason: String)
+object RejectionResponse {
+  @SuppressWarnings(Array("org.wartremover.warts.PublicInference"))
+  implicit val encoder: Encoder[RejectionResponse] =
+    deriveEncoder[RejectionResponse]
+
+  @SuppressWarnings(Array("org.wartremover.warts.PublicInference"))
+  implicit val decoder: Decoder[RejectionResponse] =
+    deriveDecoder[RejectionResponse]
+}
+
+object ApiRejectionHandler extends LazyLogging {
 
   def apply(): server.RejectionHandler =
-    server.RejectionHandler.default
-
+    RejectionHandler
+      .newBuilder()
+      .handle {
+        case MalformedQueryParamRejection(name, msg, _) ⇒
+          val response =
+            RejectionResponse(s"The query parameter '$name' was malformed:$msg")
+          complete((StatusCodes.BadRequest, response))
+      }
+      .handle {
+        case MissingQueryParamRejection(paramName) ⇒
+          val response =
+            RejectionResponse(s"Request is missing required query parameter '$paramName'")
+          complete((NotFound, response))
+      }
+      .handleAll[MethodRejection] { methodRejections ⇒
+        val names = methodRejections.map(_.supported.name)
+        val response = RejectionResponse(s"HTTP method not allowed, supported methods: ${names.mkString(", ")}")
+        complete((StatusCodes.MethodNotAllowed, response))
+      }
+      .handleNotFound {
+        val response =
+          RejectionResponse("The requested resource could not be found.")
+        complete((StatusCodes.NotFound, response))
+      }
+      .result()
 }
